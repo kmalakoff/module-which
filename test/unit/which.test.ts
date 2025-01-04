74f6b05208c2660639095fd25570304497b13320
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-03 19:28:00 -0800

    Replace module-root-sync with manual path calculation

--- test/unit/which.test.ts
+++ test/unit/which.test.ts
@@ -1,14 +1,13 @@
 import assert from 'assert';
 import path from 'path';
 import url from 'url';
-import moduleRoot from 'module-root-sync';
 import Pinkie from 'pinkie-promise';
 
 // @ts-ignore
 import which from 'module-which';
 
 const __dirname = path.dirname(typeof __filename !== 'undefined' ? __filename : url.fileURLToPath(import.meta.url));
-const root = moduleRoot(__dirname);
+const nodeModules = path.join(__dirname, '..', '..', 'node_modules');
 
 describe('which', () => {
   (() => {
@@ -28,13 +27,13 @@ describe('which', () => {
   it('finds biome', (done) => {
     which('biome', (err, biome) => {
       if (err) return done(err);
-      assert.equal(biome.indexOf(path.join(root, 'node_modules', '.bin', 'biome')), 0);
+      assert.equal(biome.indexOf(path.join(nodeModules, '.bin', 'biome')), 0);
       done();
     });
   });
 
   it('finds biome (promise)', async () => {
     const biome = await which('biome');
-    assert.equal(biome.indexOf(path.join(root, 'node_modules', '.bin', 'biome')), 0);
+    assert.equal(biome.indexOf(path.join(nodeModules, '.bin', 'biome')), 0);
   });
 });
