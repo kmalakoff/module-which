70a81e7ff757dbf8cba5f04a4f7c26f35a513966
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-03 19:28:00 -0800

    Upgrade modules

--- src/prependEnvPath.ts
+++ src/prependEnvPath.ts
@@ -1,12 +1,11 @@
 import path from 'path';
 import url from 'url';
 import envPathKey from 'env-path-key';
-import moduleRoot from 'module-root-sync';
 import prepend from 'path-string-prepend';
 import modulePaths from './modulePaths.cjs';
 
 const __dirname = path.dirname(typeof __filename === 'undefined' ? url.fileURLToPath(import.meta.url) : __filename);
-const root = moduleRoot(__dirname);
+const binPath = path.join(__dirname, '..', '..', 'node_modules', '.bin');
 
 import type { PrependResult, WhichOptions } from './types';
 
@@ -20,7 +19,7 @@ export default function prependPath(options: WhichOptions = {}): PrependResult {
   for (let i = 1; i < paths.length; i++) {
     envPath = prepend(envPath, path.join(paths[i], '.bin'));
   }
-  envPath = prepend(envPath, path.join(root, 'node_modules', '.bin'));
+  envPath = prepend(envPath, binPath);
 
   return { envPath, pathKey };
 }
