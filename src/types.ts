d6e6742dc0ba0ade72bf96667995455be1bc745d
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-03 18:23:50 -0800

    Add prependEnvPath and remove whichAll

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,12 @@
 export interface WhichOptions {
   cwd?: string;
   env?: NodeJS.ProcessEnv;
+  root?: string;
 }
 
 export type WhichCallback = (err?: Error, resolved?: string) => void;
+
+export interface PrependResult {
+  envPath: string;
+  pathKey: string;
+}
--- test/exports/import.test.mjs
+++ test/exports/import.test.mjs
@@ -1,9 +1,9 @@
 import assert from 'assert';
-import which, { whichAll } from 'module-which';
+import which, { prependEnvPath } from 'module-which';
 
 describe('exports .mjs', () => {
   it('defaults', () => {
     assert.equal(typeof which, 'function');
-    assert.equal(typeof whichAll, 'function');
+    assert.equal(typeof prependEnvPath, 'function');
   });
 });
