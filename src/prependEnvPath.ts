94741ebb77e1a07df5c26b736ffa9a00dfe28729
Kevin Malakoff <kmalakoff@gmail.com>
2025-05-19 14:25:36 -0700

    Upgrade to mandatory typescript extensions

--- src/prependEnvPath.ts
+++ src/prependEnvPath.ts
@@ -7,19 +7,18 @@ import modulePaths from './modulePaths.cjs';
 const __dirname = path.dirname(typeof __filename === 'undefined' ? url.fileURLToPath(import.meta.url) : __filename);
 const binPath = path.join(__dirname, '..', '..', 'node_modules', '.bin');
 
-import type { PrependResult, WhichOptions } from './types';
+import type { PrependResult, WhichOptions } from './types.js';
 
 export default function prependPath(options: WhichOptions = {}): PrependResult {
   const env = options.env || process.env;
-  const pathKey = envPathKey(env) || '';
+  const pathKey = envPathKey({ env }) || '';
+
   let envPath = env[pathKey] || '';
-
   const paths = modulePaths();
-
   for (let i = 1; i < paths.length; i++) {
-    envPath = prepend(envPath, path.join(paths[i], '.bin'));
+    envPath = prepend(envPath, path.join(paths[i], '.bin')) as string;
   }
-  envPath = prepend(envPath, binPath);
+  envPath = prepend(envPath, binPath) as string;
 
   return { envPath, pathKey };
 }
