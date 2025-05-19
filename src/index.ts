8066baef7ff02b32a3f32fccf32c1174e77978a4
Kevin Malakoff <kmalakoff@gmail.com>
2025-05-19 14:25:36 -0700

    Upgrade to mandatory typescript extensions

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export type * from './types';
-export { default } from './which';
-export { default as prependEnvPath } from './prependEnvPath';
+export type * from './types.js';
+export { default } from './which.js';
+export { default as prependEnvPath } from './prependEnvPath.js';
