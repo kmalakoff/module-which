c8408f3b6142294ebcc4032fe63029767ee3e48e
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-17 21:16:49 -0800

    Function overloads

--- src/which.ts
+++ src/which.ts
@@ -19,13 +19,16 @@ function worker(command, options, callback) {
 
 import type { WhichCallback, WhichOptions } from './types.ts';
 
-export default function moduleWhich(command: string, options?: WhichOptions | WhichCallback, callback?: WhichCallback): undefined | Promise<string> {
+export default function moduleWhich(command: string, callback: WhichCallback): void;
+export default function moduleWhich(command: string, options: WhichOptions, callback: WhichCallback): void;
+export default function moduleWhich(command: string, options?: WhichOptions): Promise<string>;
+export default function moduleWhich(command: string, options?: WhichOptions | WhichCallback, callback?: WhichCallback): void | Promise<string> {
   if (typeof options === 'function') {
     callback = options as WhichCallback;
     options = {};
   }
   options = options || {};
 
-  if (typeof callback === 'function') return worker(command, options, callback) as undefined;
+  if (typeof callback === 'function') return worker(command, options, callback);
   return new Promise((resolve, reject) => worker(command, options, (err, restore) => (err ? reject(err) : resolve(restore))));
 }
