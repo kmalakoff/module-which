e5d8bab285e09a703d18c2dbb4dfd81faaf1d724
Kevin Malakoff <kmalakoff@gmail.com>
2026-09-01 23:45:26 -0700

    Update Biome configuration and upgrade dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
+export { default as prependEnvPath } from './prependEnvPath.js';
 export type * from './types.js';
 export { default } from './which.js';
-export { default as prependEnvPath } from './prependEnvPath.js';
